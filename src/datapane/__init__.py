c2d0709e323592ab7194fbff34463b5558383f39
Mandeep Gill <mandeep@datapane.com>
2020-06-23 11:50:28 +0100

    Bump version: 0.6.5 → 0.6.6

--- src/datapane/__init__.py
+++ src/datapane/__init__.py
@@ -5,7 +5,7 @@ except ImportError:
     # NOTE - could use subprocess to get from git?
     __rev__ = "local"
 
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 
 # Public API re-exports
 from .client.api import (
