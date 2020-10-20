2685e37594fcd236f291fd1faf4cdfc8ae6c1fc5
Apoorva Pandey <apoorva@datapane.com>
2020-10-20 17:40:20 +0530

    Add visibility type UNLISTED (#385)
    - Add visibility type UNLISTED

--- src/datapane/client/commands.py
+++ src/datapane/client/commands.py
@@ -340,7 +340,7 @@ def report():
 # @report.command()
 # @click.argument("name")
 # @click.argument("files", type=click.Path(), nargs=-1, required=True)
-# @click.option("--visibility", type=click.Choice(["PUBLIC", "ORG", "PRIVATE"]))
+# @click.option("--visibility", type=click.Choice(["PUBLIC", "ORG", "PRIVATE", "UNLISTED"]))
 # def create(files: Tuple[str], name: str, visibility: str):
 #     """Create a Report from the provided FILES"""
 #     blocks = [api.Asset.upload_file(file=Path(f)) for f in files]
