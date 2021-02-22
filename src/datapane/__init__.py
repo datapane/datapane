d618e1493fba339dd0397fdb576319e71138d7b0
Mandeep Gill <mandeep@datapane.com>
2021-02-22 11:46:16 +0000

    Bump version: 0.9.2 → 0.10.0

--- src/datapane/__init__.py
+++ src/datapane/__init__.py
@@ -11,7 +11,7 @@ except ImportError:
     # NOTE - could use subprocess to get from git?
     __rev__ = "local"
 
-__version__ = "0.9.2"
+__version__ = "0.10.0"
 
 TEST_ENV = bool(os.environ.get("DP_TEST_ENV", ""))
 
