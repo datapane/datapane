803638032186d9382bc9f7eff56c6510edfe91ac
Mandeep Gill <mandeep@datapane.com>
2021-03-20 15:48:37 +0000

    fix(repo): Update report validation (#601)

--- src/datapane/common/report.py
+++ src/datapane/common/report.py
@@ -1,7 +1,9 @@
+import re
 import typing as t
 
 import importlib_resources as ir
 from lxml import etree
+from lxml.etree import DocumentInvalid
 
 from .utils import log
 
@@ -10,36 +12,24 @@ rng_validator = etree.RelaxNG(file=str(local_report_def / "full_schema.rng"))
 
 dp_namespace: str = "https://datapane.com/schemas/report/1/"
 
-
-def _log_validation_error(xml_str, xml_doc):
-    if not xml_str:
-        xml_str = etree.tounicode(xml_doc, pretty_print=True)
-    log.error("Error validating report document:")
-    log.error(xml_str)
+# regex to check for a xsd:ID name
+re_xml_id = re.compile(r"^[a-zA-Z_][\w.-]*$")
 
 
 def validate_report_doc(
-    xml_str: t.Optional[str] = None, xml_doc: t.Optional[etree.Element] = None, raise_exception: bool = True
+    xml_str: t.Optional[str] = None, xml_doc: t.Optional[etree.Element] = None, quiet: bool = False
 ) -> bool:
-    """Validate the model against the schema"""
-    # get the doc
+    """Validate the model against the schema, throws an etree.DocumentInvalid if not"""
+    assert xml_str or xml_doc
     if xml_str:
         xml_doc = etree.fromstring(xml_str)
-    # TODO - should remove this debug log at some point
-    # if log.isEnabledFor(logging.DEBUG):
-    #     log.debug(f"Validating\n{etree.tounicode(xml_doc, pretty_print=True)}")
 
-    # NOTE - this is messy, but works... :)
-    if raise_exception:
-        try:
-            rng_validator.assertValid(xml_doc)
-            return True
-        except Exception:
-            _log_validation_error(xml_str, xml_doc)
-            raise
-    else:
-        if rng_validator.validate(xml_doc):
-            return True
-        else:
-            _log_validation_error(xml_str, xml_doc)
-            return False
+    try:
+        rng_validator.assertValid(xml_doc)
+        return True
+    except DocumentInvalid:
+        if not xml_str:
+            xml_str = etree.tounicode(xml_doc, pretty_print=True)
+        if not quiet:
+            log.error(f"Error validating report document:\n\n{xml_str}")
+        raise
