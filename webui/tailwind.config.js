776394a9f73ce5659e6c2a385771317d45df9e50
Jake Browning <jake.reid.browning@gmail.com>
2022-03-04 11:15:29 +0000

    Add custom theme to tailwind

--- webui/tailwind.config.js
+++ webui/tailwind.config.js
@@ -1,13 +1,164 @@
+/* eslint-disable */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const { colors: defaultColors, fontFamily } = defaultTheme;
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      ...fontFamily,
+      "dp-prose": "var(--dp-font-family)",
+    },
+    colors: {
+      ...defaultColors,
+      "dp-accent-light": "var(--dp-accent-secondary-color)",
+      "dp-accent": "var(--dp-accent-color)",
+      "dp-accent-text": "var(--dp-accent-text)",
+      "dp-background": "var(--dp-bg-color)",
+      "dp-light-gray": "var(--dp-light-gray)",
+      "dp-dark-gray": "var(--dp-dark-gray)",
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.900"),
+            p: {
+              "text-align": "var(--dp-text-align)",
+              hyphens: "auto",
+            },
+            li: {
+              "text-align": "var(--dp-text-align)",
+              hyphens: "auto",
+            },
+            a: {
+              color: theme("colors.indigo.300"),
+              "&:hover": {
+                color: theme("colors.indigo.300"),
+              },
+            },
+            h1: {
+              color: theme("colors.gray.900"),
+            },
+            h2: {
+              color: theme("colors.gray.900"),
+            },
+            h3: {
+              color: theme("colors.gray.900"),
+            },
+            h4: {
+              color: theme("colors.gray.900"),
+            },
+            h5: {
+              color: theme("colors.gray.900"),
+            },
+            h6: {
+              color: theme("colors.gray.900"),
+            },
+            strong: {
+              color: theme("colors.gray.900"),
+            },
+            code: {
+              color: theme("colors.gray.900"),
+            },
+            figcaption: {
+              color: theme("colors.gray.900"),
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme("colors.gray.100"),
+            h1: {
+              color: theme("colors.gray.100"),
+            },
+            h2: {
+              color: theme("colors.gray.100"),
+            },
+            h3: {
+              color: theme("colors.gray.100"),
+            },
+            h4: {
+              color: theme("colors.gray.100"),
+            },
+            h5: {
+              color: theme("colors.gray.100"),
+            },
+            h6: {
+              color: theme("colors.gray.100"),
+            },
+            strong: {
+              color: theme("colors.gray.100"),
+            },
+            code: {
+              color: theme("colors.gray.100"),
+            },
+            figcaption: {
+              color: theme("colors.gray.300"),
+            },
+          },
+        },
+      }),
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      width: {
+        80: "20rem",
+        96: "24rem",
+        "640px": "640px",
+      },
+      minWidth: {
+        "100px": "100px",
+      },
+      maxWidth: {
+        "150px": "150px",
+      },
+      height: {
+        "480px": "480px",
+      },
+      flex: {
+        fixed: "0 0 auto",
+        "fixed-64": "0 0 16rem",
+        full: "0 0 100%",
+        "initial-grow": "1 0 0",
+        "initial-80": "0 1 20rem",
+      },
+      gridTemplateColumns: {
+        fit: "repeat(auto-fit, minmax(0, 1fr))",
+      },
+      gridTemplateRows: {
+        fit: "repeat(auto-fit, minmax(auto, auto))",
+      },
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+    },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      backgroundColor: ["active", "odd"],
+      textColor: ["active"],
+      display: ["group-hover"],
+    },
+    opacity: ["responsive", "hover", "focus", "disabled"],
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
   content: [
     // FE files
-    "./src/**/*.html",
-    "./src/**/*.tsx",
+    "./src/**/*.{vue,js,ts}",
     // BE files
     "../dp-server/src/dp/apps/dp_core/templates/**/*.html",
     "../dp-server/src/dp/apps/dp_marketing/templates/**/*.html",
