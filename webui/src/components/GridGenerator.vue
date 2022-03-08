96c91d76426e8ee2763f75c70e9df15bff4d512d
Jake Browning <jake.reid.browning@gmail.com>
2022-03-08 14:21:33 +0000

    refactor Group into own component

--- webui/src/components/GridGenerator.vue
+++ webui/src/components/GridGenerator.vue
@@ -7,37 +7,18 @@ import { BlockTree, Group, isGroup } from "../data-model/blocks";
 import { inject } from "vue";
 
 const singleBlockEmbed = inject("singleBlockEmbed");
-
-const objOrEmpty = (obj: any, n: number) => (n > 0 ? obj : {});
 const p = defineProps<{ tree: BlockTree }>();
-
-// TODO - move into Group component
-const columns = (p.tree as Group).columns;
-const gridLayoutStyle = {
-  ...objOrEmpty(
-    {
-      gridTemplateColumns: `repeat(${columns}, minmax(0, 1fr))`,
-    },
-    +columns
-  ),
-};
 </script>
 
 <template>
   <template v-if="isGroup(p.tree)">
-    <div
-      :class="[
-        'sm:grid sm:gap-4 py-4',
-        { 'grid-flow-col grid-cols-fit': !columns },
-      ]"
-      :style="gridLayoutStyle"
-    >
+    <GroupLayout :columns="p.tree.columns">
       <GridGenerator
         v-for="child in p.tree.children"
         :key="child.refId"
         :tree="child"
       ></GridGenerator>
-    </div>
+    </GroupLayout>
   </template>
   <template v-else>
     <BlockWrapper
@@ -55,9 +36,12 @@ const gridLayoutStyle = {
 
 <script lang="ts">
 import BlockWrapper from "./blocks/BlockWrapper.vue";
+import GroupLayout from "./blocks/Group.vue";
+
 export default {
   components: {
     BlockWrapper,
+    GroupLayout,
   },
 };
 </script>
---
+++ webui/src/components/blocks/Group.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const objOrEmpty = (obj: any, n: number) => (n > 0 ? obj : {});
+
+const p = defineProps<{ columns: number }>();
+
+const gridLayoutStyle = {
+  ...objOrEmpty(
+    {
+      gridTemplateColumns: `repeat(${p.columns}, minmax(0, 1fr))`,
+    },
+    +p.columns
+  ),
+};
+</script>
+
+<template>
+  <div
+    :class="[
+      'sm:grid sm:gap-4 py-4',
+      { 'grid-flow-col grid-cols-fit': !p.columns },
+    ]"
+    :style="gridLayoutStyle"
+  >
+    <slot />
+  </div>
+</template>
