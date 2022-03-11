236ad29d3cd1ecdc7ba791fd2959a70a2ae5a964
Jake Browning <jake.reid.browning@gmail.com>
2022-03-11 16:37:29 +0000

    searchable select

--- webui/src/components/layout/Select.vue
+++ webui/src/components/layout/Select.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { BlockTree, Select } from "../../data-model/blocks";
 import GridGenerator from "../GridGenerator.vue";
+import "vue-multiselect/dist/dist/vue-multiselect.esm.css";
 
 const p = defineProps<{ select: Select }>();
 
@@ -17,18 +18,20 @@ const getSectionType = (): string => {
   return children.length < 5 ? "tabs" : "dropdown";
 };
 
-const setTabNumber = (val: number) => {
-  tabNumber.value = val;
-};
-
 const sectionType = getSectionType();
+const selectSearchOptions = labels.map((label, idx) => ({
+  text: label,
+  value: idx,
+}));
+
+const tabNumbers = labels.map((_, idx) => idx);
 </script>
 
 <template>
   <div class="w-full" data-cy="section-tabs">
     <div :class="['w-full mb-2', { 'sm:hidden': sectionType === 'tabs' }]">
-      <!-- TODO - search -->
       <select
+        v-if="p.select.children.length < 10"
         id="tabs"
         name="tabs"
         class="block mb-1 w-auto focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
@@ -39,6 +42,15 @@ const sectionType = getSectionType();
           {{ label }}
         </option>
       </select>
+      <MultiSelect
+        v-else
+        v-model="tabNumber"
+        :options="tabNumbers"
+        :preselect-first="true"
+        :clear-on-select="false"
+        :allow-empty="false"
+        :custom-label="(tabNumber) => labels[tabNumber]"
+      />
     </div>
     <div :class="['hidden', { 'sm:block': sectionType === 'tabs' }]">
       <nav class="flex space-x-4 mb-2">
@@ -62,15 +74,38 @@ const sectionType = getSectionType();
     </div>
     <div>
       <GridGenerator :tree="p.select.children[tabNumber]" />
-      <slot />
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import MultiSelect from "vue-multiselect";
+
 export default {
   components: {
     GridGenerator,
+    MultiSelect,
   },
 };
 </script>
+
+<style>
+.multiselect__option--highlight {
+  background: var(--dp-accent-color);
+  color: var(--dp-accent-text);
+}
+
+.multiselect__option--selected.multiselect__option--highlight {
+  background: white;
+  color: black;
+}
+
+.multiselect__option--highlight:after {
+  display: none;
+}
+
+.multiselect__option--highlight.multiselect__option--selected {
+  background: var(--dp-accent-color);
+  color: var(--dp-accent-text);
+}
+</style>
