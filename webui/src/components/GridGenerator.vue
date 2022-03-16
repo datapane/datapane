f4bec69aec5897854ebb5b47b7b20dd67190f314
Jake Browning <jake.reid.browning@gmail.com>
2022-03-16 18:12:20 +0000

    Fix types

--- webui/src/components/GridGenerator.vue
+++ webui/src/components/GridGenerator.vue
@@ -5,26 +5,29 @@
 
 import {
   BlockTree,
-  Group,
-  isBlock,
   isGroup,
   isSelect,
+  isToggle,
+  isBlock,
 } from "../data-model/blocks";
-import BlockWrapper from "./blocks/BlockWrapper.vue";
+import { defineAsyncComponent, inject } from "vue";
 import GroupLayout from "./blocks/Group.vue";
-import { inject, defineAsyncComponent } from "vue";
+import BlockWrapper from "./blocks/BlockWrapper.vue";
 const SelectLayout = defineAsyncComponent(() => import("./layout/Select.vue"));
 
 const createGridKey = (child: BlockTree, idx: number) =>
   isBlock(child) ? `${child.refId}-${idx}` : `${child.name}-${idx}`;
 
-const singleBlockEmbed = inject("singleBlockEmbed");
+const singleBlockEmbed = inject<boolean>("singleBlockEmbed");
 const p = defineProps<{ tree: BlockTree }>();
 </script>
 
 <template>
   <template v-if="isGroup(p.tree)">
-    <GroupLayout :columns="p.tree.columns" :singleBlockEmbed="singleBlockEmbed">
+    <GroupLayout
+      :columns="p.tree.columns"
+      :singleBlockEmbed="!!singleBlockEmbed"
+    >
       <GridGenerator
         v-for="(child, idx) in p.tree.children"
         :key="createGridKey(child, idx)"
@@ -36,6 +39,9 @@ const p = defineProps<{ tree: BlockTree }>();
     <SelectLayout :select="p.tree" v-slot="slotProps">
       <GridGenerator :tree="p.tree.children[slotProps.tabNumber]" />
     </SelectLayout>
+  </template>
+  <template v-else-if="isToggle(p.tree)">
+    <div><!-- TODO --></div>
   </template>
   <template v-else>
     <BlockWrapper
