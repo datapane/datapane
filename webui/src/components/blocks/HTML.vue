134eda36c81208fdba1d9441effdb06a22a970fc
Jake Browning <jake.reid.browning@gmail.com>
2022-03-16 10:16:27 +0000

    Use uuid iframe id

--- webui/src/components/blocks/HTML.vue
+++ webui/src/components/blocks/HTML.vue
@@ -3,7 +3,9 @@ import { computed, ComputedRef, onMounted } from "vue";
 import iframeResize from "iframe-resizer/js/iframeResizer";
 import userIframeCss from "../../styles/user-iframe.css?inline";
 import contentWindowJs from "iframe-resizer/js/iframeResizer.contentWindow.js?raw";
+import { v4 as uuid4 } from "uuid";
 
+const iframeId = uuid4();
 const p = defineProps<{ html: string; sandbox?: string }>();
 
 const iframeDoc: ComputedRef<string> = computed(() => {
@@ -19,7 +21,7 @@ const iframeDoc: ComputedRef<string> = computed(() => {
 });
 
 onMounted(() => {
-  iframeResize({ log: true, checkOrigin: false }, "#my-iframe");
+  iframeResize({ checkOrigin: false }, `#${iframeId}`);
 });
 </script>
 
@@ -27,7 +29,7 @@ onMounted(() => {
   <iframe
     :srcdoc="iframeDoc"
     :sandbox="p.sandbox"
-    id="my-iframe"
+    :id="iframeId"
     width="100%"
     frameborder="0"
     data-cy="block-user-iframe"
