ca5bb085379d94a183d71849dc8f9eebf2c8deed
Jake Browning <jake.reid.browning@gmail.com>
2022-03-21 14:22:02 +0000

    Folium block

---
+++ webui/src/components/blocks/Folium.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const p = defineProps<{ iframeContent: string; singleBlockEmbed: boolean }>();
+const IFRAME_HEIGHT = "35rem";
+</script>
+
+<template>
+    <div
+        :style="{ height: IFRAME_HEIGHT }"
+        :class="[
+            'flex justify-center w-full',
+            { 'h-iframe': singleBlockEmbed },
+        ]"
+        data-cy="block-iframe"
+    >
+        <iframe
+            :style="{ border: 'none !important' }"
+            :class="['w-full', { 'h-iframe': singleBlockEmbed }]"
+            :srcdoc="p.iframeContent"
+            sandbox="allow-scripts"
+        />
+    </div>
+</template>
