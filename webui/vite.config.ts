ffdc808ec0b2067ce0c14e189cf7c09bdd70ae82
Jake Browning <jake.reid.browning@gmail.com>
2022-03-22 12:29:01 +0000

    Alias vue in rollup instead of vite

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -4,50 +4,54 @@ import replace from "@rollup/plugin-replace";
 import path from "path";
 
 module.exports = defineConfig({
-  resolve: {
-    alias: {
-      // TODO - Keep vue bundled in dev mode and add external module to rollup config
-      vue: "https://unpkg.com/vue@3.2.29/dist/vue.esm-browser.js",
-    },
-  },
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) =>
-            tag.startsWith("dp-") ||
-            tag.startsWith("x-") ||
-            tag.startsWith("revo-"),
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) =>
+                        tag.startsWith("dp-") ||
+                        tag.startsWith("x-") ||
+                        tag.startsWith("revo-"),
+                },
+            },
+        }),
+        replace({
+            include: ["node_modules/@bokeh/**/*.js"],
+            values: {
+                // shim jquery to window object for bokehjs
+                jQuery: "window.jQuery",
+            },
+        }),
+    ],
+    build: {
+        lib: {
+            // build is empty if I don't add this build.lib config
+            // TODO - Make issue on vite repo
+            formats: ["es"],
+            entry: "",
         },
-      },
-    }),
-    replace({
-      include: ["node_modules/@bokeh/**/*.js"],
-      values: {
-        // shim jquery to window object for bokehjs
-        jQuery: "window.jQuery",
-      },
-    }),
-  ],
-  build: {
-    lib: {
-      // build is empty if I don't add this build.lib config
-      // TODO - Make issue on vite repo
-      formats: ["es"],
-      entry: "",
+        rollupOptions: {
+            input: {
+                "report-component": path.resolve(
+                    __dirname,
+                    "src/entries/report.ts"
+                ),
+                "code-block": path.resolve(
+                    __dirname,
+                    "src/entries/code-block.ts"
+                ),
+                base: path.resolve(__dirname, "src/entries/base.ts"),
+            },
+            output: {
+                format: "es",
+                entryFileNames: "[name].[format].js",
+                assetFileNames: "[name].[ext]",
+                dir: path.resolve(__dirname, "dist"),
+                paths: {
+                    vue: "https://unpkg.com/vue@3.2.29/dist/vue.esm-browser.js",
+                },
+            },
+            external: ["vue"],
+        },
     },
-    rollupOptions: {
-      input: {
-        "report-component": path.resolve(__dirname, "src/entries/report.ts"),
-        "code-block": path.resolve(__dirname, "src/entries/code-block.ts"),
-        base: path.resolve(__dirname, "src/entries/base.ts"),
-      },
-      output: {
-        format: "es",
-        entryFileNames: "[name].[format].js",
-        assetFileNames: "[name].[ext]",
-        dir: path.resolve(__dirname, "dist"),
-      },
-    },
-  },
 });
