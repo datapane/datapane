aaeaeb094628648cbc6e770e25b3247b226783e0
Jake Browning <jake.reid.browning@gmail.com>
2022-03-22 13:15:16 +0000

    simplify grid-generator template

--- webui/src/components/GridGenerator.vue
+++ webui/src/components/GridGenerator.vue
@@ -16,42 +16,40 @@ const singleBlockEmbed = inject<boolean>("singleBlockEmbed");
 </script>
 
 <template>
-    <template v-if="isGroup(p.tree)">
-        <group-layout
-            :columns="p.tree.columns"
-            :single-block-embed="!!singleBlockEmbed"
-        >
-            <grid-generator
-                v-for="(child, idx) in p.tree.children"
-                :key="createGridKey(child, idx)"
-                :tree="child"
-            ></grid-generator>
-        </group-layout>
-    </template>
-    <template v-else-if="isSelect(p.tree)">
-        <select-layout :select="p.tree" v-slot="slotProps">
-            <grid-generator :tree="p.tree.children[slotProps.tabNumber]" />
-        </select-layout>
-    </template>
-    <template v-else-if="isToggle(p.tree)">
-        <toggle-layout :label="p.tree.label">
-            <grid-generator
-                v-for="(child, idx) in p.tree.children"
-                :key="createGridKey(child, idx)"
-                :tree="child"
-            ></grid-generator>
-        </toggle-layout>
-    </template>
-    <template v-else>
-        <block-wrapper
-            :caption-type="p.tree.captionType"
-            :caption="p.tree.caption"
-            :count="p.tree.count"
-        >
-            <component
-                :is="p.tree.component"
-                v-bind="p.tree.componentProps"
-            ></component>
-        </block-wrapper>
-    </template>
+    <group-layout
+        v-if="isGroup(p.tree)"
+        :columns="p.tree.columns"
+        :single-block-embed="!!singleBlockEmbed"
+    >
+        <grid-generator
+            v-for="(child, idx) in p.tree.children"
+            :key="createGridKey(child, idx)"
+            :tree="child"
+        ></grid-generator>
+    </group-layout>
+    <select-layout
+        v-else-if="isSelect(p.tree)"
+        :select="p.tree"
+        v-slot="slotProps"
+    >
+        <grid-generator :tree="p.tree.children[slotProps.tabNumber]" />
+    </select-layout>
+    <toggle-layout v-else-if="isToggle(p.tree)" :label="p.tree.label">
+        <grid-generator
+            v-for="(child, idx) in p.tree.children"
+            :key="createGridKey(child, idx)"
+            :tree="child"
+        ></grid-generator>
+    </toggle-layout>
+    <block-wrapper
+        v-else
+        :caption-type="p.tree.captionType"
+        :caption="p.tree.caption"
+        :count="p.tree.count"
+    >
+        <component
+            :is="p.tree.component"
+            v-bind="p.tree.componentProps"
+        ></component>
+    </block-wrapper>
 </template>
