00bff3a2dc29f6ec26c23eec01c25d3daacc502f
Jake Browning <jake.reid.browning@gmail.com>
2022-03-28 12:21:35 +0100

    Remove HTML entity decoding from local-report base

--- webui/src/entries/local-report.ts
+++ webui/src/entries/local-report.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import Report from "../components/Report.vue";
 import CodeBlock from "../components/blocks/Code.ce.vue";
-import he from "he";
 import "../styles/report.scss";
 import "../styles/tailwind.css";
 
@@ -9,10 +8,10 @@ window.addEventListener("DOMContentLoaded", () => {
     const windowProps = window.reportProps;
     const reportProps = {
         ...windowProps,
-        htmlHeader: he.decode(windowProps.htmlHeader),
+        htmlHeader: windowProps.htmlHeader,
         report: {
             ...windowProps.report,
-            document: he.decode(windowProps.report.document),
+            document: windowProps.report.document,
         },
     };
 
