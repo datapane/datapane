77f42b635de44833daa5a0fc0555b1749435c040
Jake Browning <jake.reid.browning@gmail.com>
2022-03-28 18:04:46 +0100

    Replace manual prose and prose-dark themes with tailwind 3 prose-inverse and dark mode

--- webui/src/components/blocks/Text.vue
+++ webui/src/components/blocks/Text.vue
@@ -15,15 +15,23 @@ const md = computed(() => (marked as any).parse(p.content));
 
 <template>
     <div
-        :class="[
-            'w-full overflow-y-hidden prose font-dp-prose',
-            { 'prose-dark': p.isLightProse },
-        ]"
+        :class="['w-full overflow-y-hidden', { dark: p.isLightProse }]"
         data-cy="block-markdown"
-        v-html="md"
-    ></div>
+    >
+        <div class="w-full prose font-dp-prose dark:prose-invert" v-html="md" />
+    </div>
 </template>
 
 <style>
 @import "highlight.js/styles/stackoverflow-light.css";
+
+pre {
+    background: #f6f6f6 !important;
+}
+
+.dark code {
+    /* hack - TW prose disabling doesn't seem to work in invert mode, so this ensures
+     non-highlighted code isn't affected by the prose-invert class */
+    color: black;
+}
 </style>
--- webui/tailwind.config.js
+++ webui/tailwind.config.js
@@ -4,200 +4,133 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const { colors: fontFamily } = defaultTheme;
 
 module.exports = {
-  theme: {
-    fontFamily: {
-      // TODO - move to extends?
-      ...fontFamily,
-      "dp-prose": "var(--dp-font-family)",
-    },
-    extend: {
-      colors: {
-        "dp-accent-light": "var(--dp-accent-secondary-color)",
-        "dp-accent": "var(--dp-accent-color)",
-        "dp-accent-text": "var(--dp-accent-text)",
-        "dp-background": "var(--dp-bg-color)",
-        "dp-light-gray": "var(--dp-light-gray)",
-        "dp-dark-gray": "var(--dp-dark-gray)",
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme("colors.gray.900"),
-            p: {
-              "text-align": "var(--dp-text-align)",
-              hyphens: "auto",
-            },
-            li: {
-              "text-align": "var(--dp-text-align)",
-              hyphens: "auto",
-            },
-            a: {
-              color: theme("colors.indigo.300"),
-              "&:hover": {
-                color: theme("colors.indigo.300"),
-              },
-            },
-            h1: {
-              color: theme("colors.gray.900"),
-            },
-            h2: {
-              color: theme("colors.gray.900"),
-            },
-            h3: {
-              color: theme("colors.gray.900"),
-            },
-            h4: {
-              color: theme("colors.gray.900"),
-            },
-            h5: {
-              color: theme("colors.gray.900"),
-            },
-            h6: {
-              color: theme("colors.gray.900"),
-            },
-            strong: {
-              color: theme("colors.gray.900"),
-            },
-            code: {
-              color: theme("colors.gray.900"),
-            },
-            figcaption: {
-              color: theme("colors.gray.900"),
-            },
-          },
+    darkMode: "class",
+    theme: {
+        fontFamily: {
+            // TODO - move to extends?
+            ...fontFamily,
+            "dp-prose": "var(--dp-font-family)",
         },
-        dark: {
-          css: {
-            color: theme("colors.gray.100"),
-            h1: {
-              color: theme("colors.gray.100"),
+        extend: {
+            colors: {
+                "dp-accent-light": "var(--dp-accent-secondary-color)",
+                "dp-accent": "var(--dp-accent-color)",
+                "dp-accent-text": "var(--dp-accent-text)",
+                "dp-background": "var(--dp-bg-color)",
+                "dp-light-gray": "var(--dp-light-gray)",
+                "dp-dark-gray": "var(--dp-dark-gray)",
             },
-            h2: {
-              color: theme("colors.gray.100"),
+            typography: {
+                DEFAULT: {
+                    // Disable prose being applied to code elements
+                    css: {
+                        pre: false,
+                        code: false,
+                        "pre code": false,
+                        "code::before": false,
+                        "code::after": false,
+                    },
+                },
             },
-            h3: {
-              color: theme("colors.gray.100"),
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
             },
-            h4: {
-              color: theme("colors.gray.100"),
+            width: {
+                80: "20rem",
+                96: "24rem",
+                "640px": "640px",
             },
-            h5: {
-              color: theme("colors.gray.100"),
+            minWidth: {
+                "100px": "100px",
             },
-            h6: {
-              color: theme("colors.gray.100"),
+            maxWidth: {
+                "150px": "150px",
             },
-            strong: {
-              color: theme("colors.gray.100"),
+            height: {
+                "480px": "480px",
             },
-            code: {
-              color: theme("colors.gray.100"),
+            flex: {
+                fixed: "0 0 auto",
+                "fixed-64": "0 0 16rem",
+                full: "0 0 100%",
+                "initial-grow": "1 0 0",
+                "initial-80": "0 1 20rem",
             },
-            figcaption: {
-              color: theme("colors.gray.300"),
+            gridTemplateColumns: {
+                fit: "repeat(auto-fit, minmax(0, 1fr))",
             },
-          },
+            gridTemplateRows: {
+                fit: "repeat(auto-fit, minmax(auto, auto))",
+            },
         },
-      }),
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-      width: {
-        80: "20rem",
-        96: "24rem",
-        "640px": "640px",
-      },
-      minWidth: {
-        "100px": "100px",
-      },
-      maxWidth: {
-        "150px": "150px",
-      },
-      height: {
-        "480px": "480px",
-      },
-      flex: {
-        fixed: "0 0 auto",
-        "fixed-64": "0 0 16rem",
-        full: "0 0 100%",
-        "initial-grow": "1 0 0",
-        "initial-80": "0 1 20rem",
-      },
-      gridTemplateColumns: {
-        fit: "repeat(auto-fit, minmax(0, 1fr))",
-      },
-      gridTemplateRows: {
-        fit: "repeat(auto-fit, minmax(auto, auto))",
-      },
+        fontSize: {
+            xs: "0.75rem",
+            sm: "0.875rem",
+            base: "1rem",
+            lg: "1.125rem",
+            xl: "1.25rem",
+            "2xl": "1.5rem",
+            "3xl": "1.875rem",
+            "4xl": "2.25rem",
+            "5xl": "3rem",
+            "6xl": "4rem",
+        },
     },
-    fontSize: {
-      xs: "0.75rem",
-      sm: "0.875rem",
-      base: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.875rem",
-      "4xl": "2.25rem",
-      "5xl": "3rem",
-      "6xl": "4rem",
+    variants: {
+        extend: {
+            backgroundColor: ["active", "odd"],
+            textColor: ["active"],
+            display: ["group-hover"],
+            opacity: ["disabled"],
+            cursor: ["disabled"],
+        },
+        opacity: ["responsive", "hover", "focus", "disabled"],
     },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ["active", "odd"],
-      textColor: ["active"],
-      display: ["group-hover"],
-      opacity: ["disabled"],
-      cursor: ["disabled"],
-    },
-    opacity: ["responsive", "hover", "focus", "disabled"],
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
-  ],
-  content: [
-    // FE files
-    "./src/**/*.{vue,js,ts}",
-    // BE files
-    // TODO - update paths when deploying
-    "../datapane-hosted/dp-server/src/dp/apps/dp_core/templates/**/*.html",
-    "../datapane-hosted/dp-server/src/dp/apps/dp_marketing/templates/**/*.html",
-    "../datapane-hosted/dp-server/src/dp/apps/dp_comments/templates/**/*.html",
-    "../datapane-hosted/dp-server/src/dp/apps/dp_org/templates/**/*.html",
-    "../datapane-hosted/dp-server/src/dp/apps/dp_public/templates/**/*.html",
-    "../datapane-hosted/dp-server/templates/**/*.html",
-    "./node_modules/@variantjs/**/*.ts",
-  ],
-  safelist: [
-    // TODO - remove redundant items
-    { pattern: /bp3-.*/ },
-    { pattern: /DayPicker.*/ },
-    { pattern: /grid-cols-.*/ },
-    { pattern: /grid-rows-.*/ },
-    { pattern: /grid-flow-.*/ },
-    { pattern: /dp-btn-.*/ },
-    "text-teal-400",
-    "text-orange-400",
-    "text-blue-400",
-    "text-green-400",
-    "text-indigo-400",
-    "text-red-400",
-    "mb-3",
-    "bg-green-100",
-    "bg-green-800",
-    "bg-red-100",
-    "bg-red-800",
-    "max-w-screen-xl",
-    "max-w-3xl",
-    "max-w-full",
-    "w-full",
-    "h-full",
-    "max-w-none",
-    "absolute",
-    "top-0",
-    "left-0",
-  ],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/aspect-ratio"),
+    ],
+    content: [
+        // FE files
+        "./src/**/*.{vue,js,ts}",
+        // BE files
+        // TODO - update paths when deploying
+        "../datapane-hosted/dp-server/src/dp/apps/dp_core/templates/**/*.html",
+        "../datapane-hosted/dp-server/src/dp/apps/dp_marketing/templates/**/*.html",
+        "../datapane-hosted/dp-server/src/dp/apps/dp_comments/templates/**/*.html",
+        "../datapane-hosted/dp-server/src/dp/apps/dp_org/templates/**/*.html",
+        "../datapane-hosted/dp-server/src/dp/apps/dp_public/templates/**/*.html",
+        "../datapane-hosted/dp-server/templates/**/*.html",
+        "./node_modules/@variantjs/**/*.ts",
+    ],
+    safelist: [
+        // TODO - remove redundant items
+        { pattern: /bp3-.*/ },
+        { pattern: /DayPicker.*/ },
+        { pattern: /grid-cols-.*/ },
+        { pattern: /grid-rows-.*/ },
+        { pattern: /grid-flow-.*/ },
+        { pattern: /dp-btn-.*/ },
+        "text-teal-400",
+        "text-orange-400",
+        "text-blue-400",
+        "text-green-400",
+        "text-indigo-400",
+        "text-red-400",
+        "mb-3",
+        "bg-green-100",
+        "bg-green-800",
+        "bg-red-100",
+        "bg-red-800",
+        "max-w-screen-xl",
+        "max-w-3xl",
+        "max-w-full",
+        "w-full",
+        "h-full",
+        "max-w-none",
+        "absolute",
+        "top-0",
+        "left-0",
+    ],
 };
