eaff14c698b4ac69d762e1c922b491073eda7d7f
Jake Browning <jake.reid.browning@gmail.com>
2022-04-13 15:32:44 +0100

    add tracking

--- webui/src/entries/report.ts
+++ webui/src/entries/report.ts
@@ -10,6 +10,11 @@ declare global {
     interface Window {
         dpLocal: boolean;
         reportProps?: any;
+        posthog: any;
+        hasPosthog: any;
+        dpAuthorId: string;
+        dpReportId: string;
+        dpLocalViewEvent: any;
     }
 }
 
