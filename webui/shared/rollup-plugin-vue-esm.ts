ab1c07bb7d532a52e2473eaabf53e09858fba09b
Jake Browning <jake.reid.browning@gmail.com>
2022-04-27 16:22:02 +0100

    fix rollup vue esm plugin

--- webui/shared/rollup-plugin-vue-esm.ts
+++ webui/shared/rollup-plugin-vue-esm.ts
@@ -9,33 +9,25 @@ import fs from "fs";
 const VUE_DEV_FNAME = "vue.esm-browser.js";
 const VUE_PROD_FNAME = "vue.esm-browser.prod.js";
 
-const findPath = async (subpath: string) => {
-    /**
-     * Travel upwards through tree until subpath is found
-     */
-    let dir = path.resolve(__dirname);
-    while (true) {
-        const fullPath = path.resolve(dir, subpath);
-        if (fs.existsSync(fullPath)) {
-            return fullPath;
-        } else {
-            dir = path.resolve(dir, "../");
-        }
-    }
-};
-
 export default () => ({
     name: "rollup-plugin-vue-esm",
     buildEnd: async () => {
-        const vuePath = await findPath("node_modules/vue/dist");
-        const distPath = await findPath("dist");
-        await fs.promises.copyFile(
-            path.resolve(vuePath, VUE_DEV_FNAME),
-            path.resolve(distPath, VUE_DEV_FNAME)
-        );
-        await fs.promises.copyFile(
-            path.resolve(vuePath, VUE_PROD_FNAME),
-            path.resolve(distPath, VUE_PROD_FNAME)
-        );
+        const vuePath = path.resolve(__dirname, "../node_modules/vue/dist");
+        const distPath = path.resolve(__dirname, "../dist");
+
+        if (!fs.existsSync(distPath)) {
+            await fs.promises.mkdir(distPath);
+        }
+
+        await Promise.all([
+            fs.promises.copyFile(
+                path.resolve(vuePath, VUE_DEV_FNAME),
+                path.resolve(distPath, VUE_DEV_FNAME)
+            ),
+            fs.promises.copyFile(
+                path.resolve(vuePath, VUE_PROD_FNAME),
+                path.resolve(distPath, VUE_PROD_FNAME)
+            ),
+        ]);
     },
 });
