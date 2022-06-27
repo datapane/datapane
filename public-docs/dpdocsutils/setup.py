6b27edac056901b877f9867cb54d22cce273639f
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-06-27 16:36:49 +0100

    feat: Added report to image support with puppeteer

---
+++ public-docs/dpdocsutils/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="dpdocsutils",
+    version="0.0.1",
+    description="dp docs utils",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
