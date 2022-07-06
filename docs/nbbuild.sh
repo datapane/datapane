3a792fa95d710e9151b27b619f792597658cae24
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-07-06 18:47:00 +0100

    chore: Notebook builder now preserves metadata

--- docs/nbbuild.sh
+++ docs/nbbuild.sh
@@ -1,6 +1,6 @@
 for f in $(find docs -type f -name '*.ipynb' ! -path '*.ipynb_checkpoints*')
     do 
         cd ${f%/*};
-        poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=-1 --execute ${f##*/};
+        poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=-1 --ClearMetadataPreprocessor.enabled=True --ClearMetadataPreprocessor.preserve_cell_metadata_mask tags --execute ${f##*/};
         cd -;
 done
