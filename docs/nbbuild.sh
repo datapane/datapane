fc06ad74c9e0d3823482b713c2b6579b8ab2979b
Anthony <anthony@datapane.com>
2022-07-28 17:02:38 +0100

    fix: rimraff the the docs directory to avoid stale artifacts from deleted docs

--- docs/nbbuild.sh
+++ docs/nbbuild.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# remove artifacts before building
+find docs -type f \( -name '*.html' -o -name '*-preview.png' \) -delete
+
 find docs -type f -name '*.ipynb' -not -path '*.ipynb_checkpoints*' -print0 | while IFS= read -r -d '' f; do
     cd "${f%/*}"
     poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=-1 --ClearMetadataPreprocessor.enabled=True --ClearMetadataPreprocessor.preserve_cell_metadata_mask tags --execute "${f##*/}"
