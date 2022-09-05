51496a3f50fd9da14256f0cca695747b6f31069a
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-09-05 17:49:18 +0100

    chore: Add args parsing tests for library mode

---
+++ projects/python-client/tests/client/e2e/test_library.py
@@ -0,0 +1,48 @@
+import subprocess
+import sys
+import os
+
+
+def test_arbitrary_arguments_from_cli():
+    """Running the pytest command with arguments for -p
+    to make sure Datapane doesn't process them."""
+    test_file_path = os.path.realpath(__file__)
+    test_command = [
+        sys.executable,
+        "-m",
+        "pytest",
+        test_file_path,
+        "-k",
+        "test_pytest_from_cli",
+        "-p",
+        "no:warnings",
+    ]
+
+    p = subprocess.run(test_command, capture_output=True)
+
+    assert p.returncode == 0, str(p.stderr).replace("\\n", "\n")
+
+
+def test_no_arguments_from_cli():
+    """Running the pytest command without arguments for -p."""
+    test_file_path = os.path.realpath(__file__)
+    test_command = [
+        sys.executable,
+        "-m",
+        "pytest",
+        test_file_path,
+        "-k",
+        "test_pytest_from_cli",
+    ]
+
+    p = subprocess.run(test_command)
+
+    assert p.returncode == 0
+
+
+def test_pytest_from_cli():
+    """This test is run by keyword expression from other CLI tests.
+    It explicitly imports datapane to ensure __init__ is always executed."""
+    import datapane as dp
+
+    assert True
