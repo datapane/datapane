6610a1762ecf0acee50c535eb482f0a51af5724a
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-09-29 17:47:46 +0100

    chore: Document block_to_iframe

--- projects/python-client/src/datapane/client/api/ipython_utils.py
+++ projects/python-client/src/datapane/client/api/ipython_utils.py
@@ -9,7 +9,14 @@ Datapane helper functions to improve the Datapane UX in Jupyter notebooks
 
 
 def block_to_iframe(block: Block) -> str:
-    """Convert a Block to HTML, placed within an iFrame"""
+    """Convert a Block to HTML, placed within an iFrame
+
+    Args:
+        block: Datapane Block to convert
+
+    Returns:
+        HTML string for the block
+    """
     # importing within function scope to avoid circular dependency
     from .report.core import App
 
