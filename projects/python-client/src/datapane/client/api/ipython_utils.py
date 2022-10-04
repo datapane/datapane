7f2372d1866c0444d034310b333ee2c6b2658230
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-10-04 12:17:53 +0100

    chore: Add markdown and input to code support

--- projects/python-client/src/datapane/client/api/ipython_utils.py
+++ projects/python-client/src/datapane/client/api/ipython_utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import json
 import typing
 
 if typing.TYPE_CHECKING:
@@ -28,6 +29,19 @@ def block_to_iframe(block: BaseElement) -> str:
     return block_html_string
 
 
+def cell_to_block(cell: dict) -> BaseElement:
+    """Convert a Jupyter notebook cell to a Datapane Block
+
+    Args:
+        cell: Jupyter notebook cell dict
+
+    Returns:
+        Datapane Block
+    """
+    # TODO
+    pass
+
+
 def cells_to_blocks(jupyter_output_cache: dict) -> list:
     """Convert Jupyter notebook cells to a list of Datapane Blocks
 
@@ -37,5 +51,31 @@ def cells_to_blocks(jupyter_output_cache: dict) -> list:
     Returns:
         List of Datapane Blocks
     """
+    import ipynbname
+
+    nb_path = ipynbname.path()
+
     blocks = []
+    notebook_json = json.loads(open(nb_path, encoding="utf-8").read())
+
+    for cell in notebook_json["cells"]:
+        tags = cell["metadata"].get("tags", [])
+
+        if cell["cell_type"] == "markdown":
+            from .report.blocks import Text
+
+            block = Text("".join(cell["source"]))
+            blocks.append(block)
+
+        elif cell["cell_type"] == "code":
+            from .report.blocks import Code
+
+            if "show-code" in tags:
+                block = Code("".join(cell["source"]))
+                blocks.append(block)
+
+            block = cell_to_block(cell)
+            if block:
+                blocks.append(block)
+
     return blocks
