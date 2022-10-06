c84b9f82660ffe57b366f1f51e98b9c2fa1e6608
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-10-06 14:33:29 +0100

    IPython integration - displaying blocks in Jupyter notebooks (#297)
    * chore: Add IPython rich display method to blocks
    
    * chore: Add app template for rich IPython display
    
    * chore: Add processor to stringify apps
    
    * chore: Add stringify method to App
    
    * chore: Add IPython integration to BaseElement

--- python-client/src/datapane/client/api/report/blocks.py
+++ python-client/src/datapane/client/api/report/blocks.py
@@ -27,6 +27,7 @@ from datapane.common.report import get_embed_url, is_valid_id, mk_attribs
 
 from ..common import DPTmpFile
 from ..dp_object import save_df
+from ..ipython_utils import block_to_iframe
 
 E = ElementMaker()  # XML Tag Factory
 
@@ -133,6 +134,14 @@ class BaseElement(ABC):
 
     def _add_attributes(self, **kwargs):
         self._attributes.update(mk_attribs(**kwargs))
+
+    def _ipython_display_(self):
+        """Display the block as a side effect within a Jupyter notebook"""
+        from IPython.display import HTML, display
+
+        block_html_string = block_to_iframe(self)
+
+        display(HTML(block_html_string))
 
     @abstractmethod
     def _to_xml(self, s: BuilderState) -> BuilderState:
