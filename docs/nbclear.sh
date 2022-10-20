6c5828e1b2be4a419354d7d7ed9099cdc4fd7394
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-10-20 10:06:24 +0100

    chore: Update nbclear comment for clarity

--- docs/nbclear.sh
+++ docs/nbclear.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # clear all output from notebooks in-place
+# this is not necessary before commit because the git filter does this automatically
 find docs -type f -name '*.ipynb' -not -path '*.ipynb_checkpoints*' -print0 | while IFS= read -r -d '' f; do
     cd "${f%/*}"
     poetry run jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --ClearMetadataPreprocessor.preserve_cell_metadata_mask tags --inplace "${f##*/}"
