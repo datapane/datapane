8bbaf28ac38c60a425a75bbb96a5b971af38db73
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-10-20 10:05:14 +0100

    chore: Add nbclear script for manual clearing of working Docs

---
+++ docs/nbclear.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+# clear all output from notebooks in-place
+find docs -type f -name '*.ipynb' -not -path '*.ipynb_checkpoints*' -print0 | while IFS= read -r -d '' f; do
+    cd "${f%/*}"
+    poetry run jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --ClearMetadataPreprocessor.preserve_cell_metadata_mask tags --inplace "${f##*/}"
+    cd -
+done
