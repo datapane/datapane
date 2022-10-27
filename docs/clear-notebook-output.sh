194b895092de1a7bf69daf17c19333b5f8575924
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-10-27 16:50:45 +0100

    Update docs/clear-notebook-output.sh
    Co-authored-by: Anthony King <anthony@datapane.com>

--- docs/clear-notebook-output.sh
+++ docs/clear-notebook-output.sh
@@ -4,6 +4,14 @@ set -euo pipefail
 
 
 # pass the stdin directly to jq (<&0)
-exec jq --indent 1 \
-  "(.cells[] | select(has(\"outputs\")) | .outputs) = [] | (.cells[] | select(has(\"execution_count\")) | .execution_count) = null | .metadata = {\"language_info\": {\"name\": \"python\"}} | .cells[].metadata |= if has(\"tags\") then {tags} else {} end" \
-  <&0
+exec jq --indent 1 "$(cat <<'EOF'
+    # clear the outputs, if they're set
+    (.cells[] | select(has("outputs")) | .outputs) = []
+    # clear execution_count, if it's set
+    | (.cells[] | select(has("execution_count")) | .execution_count) = null
+    # unconditionally set the notebook metadata to be static
+    | .metadata = {"language_info": {"name": "python"}}
+    # allow tags, nothing else
+    | .cells[].metadata |= if has("tags") then {tags} else {} end
+EOF
+)" <&0
