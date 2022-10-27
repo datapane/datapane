eb6c2d1f85663bce0adafa388e778496cc962751
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-10-27 16:14:24 +0100

    chore: Update clear-notebook-output to support stdin

--- docs/clear-notebook-output.sh
+++ docs/clear-notebook-output.sh
@@ -1,1 +1,9 @@
-jq --indent 1 "(.cells[] | select(has(\"outputs\")) | .outputs) = [] | (.cells[] | select(has(\"execution_count\")) | .execution_count) = null | .metadata = {\"language_info\": {\"name\": \"python\"}} | .cells[].metadata |= if has(\"tags\") then {tags} else {} end" $1
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+# pass the stdin directly to jq (<&0)
+exec jq --indent 1 \
+  "(.cells[] | select(has(\"outputs\")) | .outputs) = [] | (.cells[] | select(has(\"execution_count\")) | .execution_count) = null | .metadata = {\"language_info\": {\"name\": \"python\"}} | .cells[].metadata |= if has(\"tags\") then {tags} else {} end" \
+  <&0
