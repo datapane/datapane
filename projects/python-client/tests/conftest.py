f2d6bd893b7686c4a18f1550439c545c2035f012
Richard Terry <git@radiac.net>
2022-10-31 17:00:04 +0000

    Change config to use configparser

--- projects/python-client/tests/conftest.py
+++ projects/python-client/tests/conftest.py
@@ -1,15 +1,28 @@
+import pytest
+
+from datapane.client import config
 from datapane.common import DPMode, _setup_dp_logging, set_dp_mode
 
-# run this at global level as want debug logging for all tests
-set_dp_mode(DPMode.SCRIPT)
-_setup_dp_logging(verbosity=2)
 
+@pytest.fixture(autouse=True)
+def dp_setup(request, monkeypatch, tmp_path):
+    """
+    Set up the common environment for each test
 
-# @pytest.fixture(scope="session")
-# def dp_init():
-#     from datapane.common import DPMode, _setup_dp_logging, set_dp_mode
-#     # init API with full debug logging
-#     set_dp_mode(DPMode.SCRIPT)
-#     _setup_dp_logging(verbosity=2)
-#     yield
-#     print("closing dp_init")
+    Skip optional setup tasks with::
+
+        @pytest.mark.skip_setup
+    """
+    # Monkeypatch config file into a tmp dir
+    config_dir = tmp_path / "config"
+    config_dir.mkdir(parents=True)
+    monkeypatch.setattr(config, "APP_DIR", config_dir)
+    monkeypatch.setattr(config, "CONFIG_PATH", config_dir / config.CONFIG_FILENAME)
+
+    # Init API with full debug logging
+    set_dp_mode(DPMode.SCRIPT)
+    _setup_dp_logging(verbosity=2)
+
+    # Optional setup
+    if "skip_setup" not in request.keywords:
+        config.init()
