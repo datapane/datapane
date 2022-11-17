d0effde9d374ff0522458ed45068a5c18fc1e137
Dr. Shahin Rostami <hello@shahinrostami.com>
2022-11-17 15:38:00 +0000

    Simplify Notebook to App API (#335)
    * chore: Remove the need for the Out parameter
    * docs: Update Jupyter integration docs for new API
    * chore: Add from_notebook to App API
    * docs: Update Jupyter integration API
    * chore: Removing caller_name ahead of notebook/cache check

--- projects/python-client/src/datapane/client/api/ipython_utils.py
+++ projects/python-client/src/datapane/client/api/ipython_utils.py
@@ -145,7 +145,7 @@ def output_cell_to_block(cell: dict, ipython_output_cache: dict) -> typing.Optio
 
 
 @capture_event("IPython Cells to Blocks")
-def cells_to_blocks(ipython_output_cache: dict, opt_out: bool = True) -> typing.List[BaseElement]:
+def cells_to_blocks(opt_out: bool = True) -> typing.List[BaseElement]:
     """Convert IPython notebook cells to a list of Datapane Blocks
 
     Recognized cell tags:
@@ -155,10 +155,16 @@ def cells_to_blocks(ipython_output_cache: dict, opt_out: bool = True) -> typing.
 
     ..note:: IPython output caching must be enabled for this function to work. It is enabled by default.
     """
+    from IPython import get_ipython
+
     display_msg("Converting cells to blocks.")
     display_msg(
         "Please ensure all cells in the notebook have been executed and then saved before running this command."
     )
+
+    ip = get_ipython()
+    user_ns = ip.user_ns
+    ipython_output_cache = user_ns["_oh"]
 
     notebook_json = get_notebook_json()
 
@@ -181,14 +187,13 @@ def cells_to_blocks(ipython_output_cache: dict, opt_out: bool = True) -> typing.
                     code_block: BaseElement = Code("".join(cell["source"]))
                     blocks.append(code_block)
 
-                if cells_to_blocks.__name__ not in "".join(cell["source"]):
-                    output_block = output_cell_to_block(cell, ipython_output_cache)
-                    if output_block:
-                        blocks.append(output_block)
-                    elif "dp-include" in tags:
-                        display_msg(
-                            f'Cell output of type {type(ipython_output_cache.get(cell["execution_count"]))} not supported. Skipping.',
-                        )
+                output_block = output_cell_to_block(cell, ipython_output_cache)
+                if output_block:
+                    blocks.append(output_block)
+                elif "dp-include" in tags:
+                    display_msg(
+                        f'Cell output of type {type(ipython_output_cache.get(cell["execution_count"]))} not supported. Skipping.',
+                    )
 
     if not blocks:
         display_msg("No blocks found.")
