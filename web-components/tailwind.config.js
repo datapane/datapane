117af5859389b6e1d2efa851f216303f1175e0fb
Anthony King <anthony@datapane.com>
2022-11-18 10:29:15 +0000

    fix: update tailwind reference to dp-server

--- web-components/tailwind.config.js
+++ web-components/tailwind.config.js
@@ -3,12 +3,12 @@
 const config = require("./base.tailwind.config");
 
 config.content = [
-    "../../../dp-server/src/dp/apps/dp_core/templates/**/*.html",
-    "../../../dp-server/src/dp/apps/dp_marketing/templates/**/*.html",
-    "../../../dp-server/src/dp/apps/dp_comments/templates/**/*.html",
-    "../../../dp-server/src/dp/apps/dp_org/templates/**/*.html",
-    "../../../dp-server/src/dp/apps/dp_public/templates/**/*.html",
-    "../../../dp-server/templates/**/*.html",
+    "../dp-server/src/dp/apps/dp_core/templates/**/*.html",
+    "../dp-server/src/dp/apps/dp_marketing/templates/**/*.html",
+    "../dp-server/src/dp/apps/dp_comments/templates/**/*.html",
+    "../dp-server/src/dp/apps/dp_org/templates/**/*.html",
+    "../dp-server/src/dp/apps/dp_public/templates/**/*.html",
+    "../dp-server/templates/**/*.html",
 ];
 
 module.exports = config;
