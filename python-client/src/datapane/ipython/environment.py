f93594fe0483c4ba0be80c0e5e969b8070ecb1f0
Anthony King <anthony@datapane.com>
2023-02-09 20:00:31 +0000

    chore(ipython/env): Restructure class heirachy, expose a getter
    - Python < IPython, but is still effectively Python for our intentions
    - IPython < IPythonZMQ is IPython with a remote kernel for execution
    - IPythonZMQ < Jupyter, includes special handling for Notebook Sessions
    - (IPythonZMQ or Jupyter) < VSCode, special as it runs in both modes
    - Codespaces..., special as it deals with remote kernels
    
    Papermill is added, to provide a convenient execution environment for servers or rendering notebooks from CLI.
    
    a memoized 'get_environment' method has been added, to defer loading as much as possible.

--- python-client/src/datapane/ipython/environment.py
+++ python-client/src/datapane/ipython/environment.py
@@ -4,41 +4,64 @@ import io
 import json
 import os
 import sys
+import typing as t
+from functools import cache, cached_property
 from pathlib import Path
 
+from datapane.client import log
 from datapane.client.exceptions import DPClientError
 
 from .exceptions import NotebookException
 
+if t.TYPE_CHECKING:
+    from IPython.core.interactiveshell import InteractiveShell
+
+__all__ = ("get_environment",)
+
+
+@cache
+def get_environment() -> PythonEnvironment:
+    """Returns the current IPython environment"""
+    env = _get_environment()
+    log.info("Detected IPython environment: %s", env.name)
+    return env
+
+
+def _get_ipython() -> t.Optional[InteractiveShell]:
+    try:
+        return get_ipython()  # type: ignore
+    except NameError:
+        from IPython import get_ipython
+
+        return get_ipython()
+
 
 def is_zqm_interactive_shell() -> bool:
     """Check if we are running in a ZMQ interactive shell"""
-    try:
-        from IPython import get_ipython
-
-        return bool(get_ipython().__class__.__name__ == "ZMQInteractiveShell")
-    except NameError:
-        return False
+    return _get_ipython().__class__.__name__ == "ZMQInteractiveShell"
 
 
 def is_terminal_interactive_shell() -> bool:
     """Check if we are running in a terminal interactive shell"""
-    try:
-        from IPython import get_ipython
-
-        return bool(get_ipython().__class__.__name__ == "TerminalInteractiveShell")
-    except NameError:
-        return False
+    return _get_ipython().__class__.__name__ == "TerminalInteractiveShell"
 
 
-def get_environment() -> PythonEnvironment:
+def get_ipython_user_ns() -> dict:
+    return _get_ipython().user_ns
+
+
+def _get_environment() -> PythonEnvironment:
     """Determines the current IPython environment and returns an instance of the appropriate class"""
+
+    # TODO: change this to be a list, and put the logic in a `.supported()` method
     if "google.colab" in sys.modules:
         return ColabEnvironment()
     elif {"CODESPACES", "JUPYTERLAB_PATH", "VSCODE_CWD"}.issubset(set(os.environ)):
         return CodespacesVSCodeJupyterEnvironment()
     elif {"CODESPACES", "JUPYTERLAB_PATH", "JPY_PARENT_PID"}.issubset(set(os.environ)):
         return CodespacesJupyterLabEnvironment()
+    elif is_zqm_interactive_shell() and "PAPERMILL_OUTPUT_PATH" in get_ipython_user_ns():
+        return PapermillEnvironment()
     elif is_zqm_interactive_shell() and "VSCODE_PID" in os.environ:
         return VSCodeJupyterEnvironment()
     elif is_zqm_interactive_shell() and {"JPY_SESSION_NAME", "JPY_PARENT_PID"}.issubset(set(os.environ)):
@@ -64,24 +87,52 @@ class PythonEnvironment:
     support_rich_display: bool = True
     supports_ipywidgets: bool = False
 
+    get_ipython = staticmethod(_get_ipython)
+
     def get_notebook_json(self) -> dict:
-        raise DPClientError("The current notebook environment is not supported.")
+        try:
+            path = self._get_notebook_path()
+            with open(path, encoding="utf-8") as f:
+                notebook_json = json.load(f)
+        except FileNotFoundError:
+            raise DPClientError(
+                "Notebook not found. This command must be executed from within a Jupyter notebook environment."
+            )
+        return notebook_json
 
-
-class IPythonZQMEnvironment(PythonEnvironment):
-    name = "IPython ZQM Environment"
-    is_notebook_environment = True
-
-
-class IPythonTerminalEnvironment(PythonEnvironment):
-    name = "IPython Terminal Environment"
-    is_notebook_environment = False
+    def _get_notebook_path(self) -> Path:
+        """Get the path to the current notebook"""
+        raise FileNotFoundError("Could not discover notebook path")
 
 
 class UnrecognizedEnvironment(PythonEnvironment):
     name = "Unrecognized Environment"
     is_notebook_environment = False
     can_open_links_from_python = False
+
+
+class IPythonTerminalEnvironment(PythonEnvironment):
+    name = "IPython Terminal Environment"
+
+
+class IPythonZMQEnvironment(IPythonTerminalEnvironment):
+    name = "IPython ZMQ Environment"
+    is_notebook_environment = True
+
+    def _get_notebook_path(self) -> Path:
+        # added in ipykernel v6.21
+        # https://github.com/ipython/ipykernel/pull/1078
+        user_ns = get_ipython_user_ns()
+
+        # If it's an actual path, it'll always be absolute
+        if (path := user_ns.get("__file__", None)) and path.startswith("/"):
+            return Path(path)
+
+        return super()._get_notebook_path()
+
+
+class UnsupportedNotebookEnvironment(IPythonZMQEnvironment):
+    name = "Unsupported Notebook Environment"
 
 
 class PyCharmEnvironment(PythonEnvironment):
@@ -93,38 +144,34 @@ class VSCodeEnvironment(PythonEnvironment):
     name = "VSCode Environment"
 
 
-class UnsupportedNotebookEnvironment(IPythonZQMEnvironment):
-    name = "Unsupported Notebook Environment"
-
-
-class JupyterLabEnvironment(IPythonZQMEnvironment):
-    name = "JupyterLab Environment"
+class JupyterEnvironment(IPythonZMQEnvironment):
+    name = "Jupyter Environment"
     supports_ipywidgets = True
 
-    def get_notebook_json(self) -> dict:
-        """Get the JSON for the current Jupyter notebook"""
-        import ipynbname
+    def _get_notebook_path(self):
+        """Get the path for the current Jupyter notebook"""
 
+        # ipynbname plays with the session API, so see if ipykernel gave us a better option
         try:
-            nb_path = ipynbname.path()
-            with open(nb_path, encoding="utf-8") as f:
-                notebook_json = json.load(f)
-        except IndexError:
-            raise DPClientError("Environment not supported.")
+            return super()._get_notebook_path()
         except FileNotFoundError as e:
-            raise DPClientError(
-                "Notebook not found. This command must be executed from within a Jupyter notebook environment."
-            ) from e
+            import ipynbname
 
-        return notebook_json
+            try:
+                return ipynbname.path()
+            except IndexError:
+                raise e
 
 
-class JupyterNotebookEnvironment(JupyterLabEnvironment):
+class JupyterLabEnvironment(JupyterEnvironment):
+    name = "JupyterLab Environment"
+
+
+class JupyterNotebookEnvironment(JupyterEnvironment):
     name = "Jupyter Notebook Environment"
-    supports_ipywidgets = True
 
 
-class VSCodeJupyterEnvironment(IPythonZQMEnvironment):
+class VSCodeJupyterEnvironment(JupyterEnvironment):
     name = "VSCode Jupyter Environment"
 
     # Attempting to use IPyWidgetsControllerUI from within VSCode Jupyter extension
@@ -133,40 +180,23 @@ class VSCodeJupyterEnvironment(IPythonZQMEnvironment):
     # - once it has failed once, it can require restarting VSCode to fix things.
     # So it's better to fall back to something that seems to work better.
     #
-    # supports_ipywidgets = False
+    supports_ipywidgets = False
 
-    def get_notebook_json(self) -> dict:
-        """Get the JSON for the current VSCode notebook"""
-        from IPython import get_ipython
+    def _get_notebook_path(self) -> Path:
+        user_ns = get_ipython_user_ns()
+        if vsc_path := user_ns.get("__vsc_ipynb_file__", None):
+            return Path(vsc_path)
 
-        ip = get_ipython()
-
-        if "__vsc_ipynb_file__" in ip.user_ns:
-            nb_path = ip.user_ns["__vsc_ipynb_file__"]
-        else:
-            import ipynbname
-
-            try:
-                nb_path = ipynbname.path()
-
-                # VSCode path name in sesssion is suffixed with -jvsc-[identifier]
-                nb_path = Path(str(nb_path).split("-jvsc-")[0] + ".ipynb")
-            except Exception as e:
-                raise DPClientError("Environment not supported.") from e
-
-        try:
-            with open(nb_path, encoding="utf-8") as f:
-                notebook_json = json.load(f)
-
-        except FileNotFoundError as e:
-            raise DPClientError(
-                "Notebook not found. This command must be executed from within a notebook environment."
-            ) from e
-
-        return notebook_json
+        # VSCode path name in sesssion may be suffixed with -jvsc-[identifier]
+        # https://github.com/microsoft/vscode-jupyter/blob/113c3e54ac1d3cb81ab6473d1a5fa4a20cce4755/src/kernels/helpers.ts#L149-L168
+        sessioned_nb_path = super()._get_notebook_path()
+        suffix = sessioned_nb_path.suffix
+        sans_suffix = sessioned_nb_path.with_suffix("")
+        nb_path = Path(str(sans_suffix).split("-jvsc-")[0]).with_suffix(suffix)
+        return nb_path
 
 
-class ColabEnvironment(IPythonZQMEnvironment):
+class ColabEnvironment(IPythonZMQEnvironment):
     name = "Google Colab Environment"
 
     def get_notebook_json(self) -> dict:
@@ -212,4 +242,19 @@ class CodespacesJupyterLabEnvironment(JupyterLabEnvironment):
     can_open_links_from_python = False
 
 
+class PapermillEnvironment(IPythonZMQEnvironment):
+    name = "Papermill Environment"
+
+    def _get_notebook_path(self) -> Path:
+        user_ns = get_ipython_user_ns()
+        if path := user_ns.get("PAPERMILL_OUTPUT_PATH", None):
+            return Path(path)
+        return super()._get_notebook_path()
+
+    @cached_property
+    def support_rich_display(self) -> bool:  # type: ignore[override]
+        return get_ipython_user_ns().get("DP_NOTEBOOK_RUNNER", False)
+
+
+# TODO: remove
 environment = get_environment()
