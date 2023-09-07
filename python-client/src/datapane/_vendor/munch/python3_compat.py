9dfff43148cd8db1dc84bb71458e152cc43c5672
Anthony King <anthony@datapane.com>
2023-09-07 14:38:16 +0100

    drop munch as a vendored dependency
    It's unused, so just dropping it.

--- python-client/src/datapane/_vendor/munch/python3_compat.py
+++
@@ -1,6 +0,0 @@
-from six import u, iteritems, iterkeys # pylint: disable=unused-import
-try:
-    from collections.abc import Mapping  # pylint: disable=unused-import
-except ImportError:
-    # Legacy Python
-    from collections.abc import Mapping  # pylint: disable=unused-import
